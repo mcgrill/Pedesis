c8fea79e920286638153f56e9107590e07b3dad1
Nicholas McGill <technick29@seas054.wlan.seas.upenn.edu>
2011-01-15 00:36:34 -0500

    Hello World

--- src/com/pedesis/pedesis.java
+++ src/com/pedesis/pedesis.java
@@ -2,6 +2,7 @@ package com.pedesis;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class pedesis extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,8 @@ public class pedesis extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Android");
+        setContentView(tv);
     }
 }
